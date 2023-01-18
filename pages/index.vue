95fdef481086cf753a2634bf59b0efb3ef2ad2be
Clissman J. Camacho <clissmancamacho@gmail.com>
2023-01-18 17:42:17 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      accusamus quis ratione nihil? Sint odio fugit in natus perspiciatis
+      dolore.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      accusamus quis ratione nihil? Sint odio fugit in natus perspiciatis
+      dolore.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      accusamus quis ratione nihil? Sint odio fugit in natus perspiciatis
+      dolore.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      accusamus quis ratione nihil? Sint odio fugit in natus perspiciatis
+      dolore.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      accusamus quis ratione nihil? Sint odio fugit in natus perspiciatis
+      dolore.
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
